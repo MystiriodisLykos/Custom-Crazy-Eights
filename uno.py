36c3a84c06fff92ef2401e939cdcb424629a1967
MystiriodisLykos <MystiriodisLykos@gmail.com>
2017-04-27 14:43:27 -0600

    Draw test2
    undid a lot of things

--- uno.py
+++ uno.py
@@ -3,7 +3,6 @@ import gevent
 import json
 from flask import Flask, render_template
 from flask_sockets import Sockets
-from random import shuffle
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
@@ -21,15 +20,9 @@ class Card:
 
 
 class UnoGame(object):
-    colors = ['red', 'blue', 'green', 'yello']
-    values = [str(x) for x in range(1, 10)] + ['skip'] + ['+2'] + ['reverse']
-
     def __init__(self):
         self.players = {}
-        self.cards = shuffle([[Card(c, v) for v in UnoGame.values]
-                              for c in UnoGame.colors]*2 +
-                             [[Card(c, '0') for c in UnoGame.colors]] +
-                             [[Card('wild', v) for v in ['', '+4']]])
+        self.cards = [Card('red', '+2')]
 
     def add(self, ws, name):
         self.players[name] = {'ws': ws}
@@ -37,8 +30,7 @@ class UnoGame(object):
     def draw(self, name):
         try:
             card = self.cards[0].dictionary()
-            # del self.cards[0]
-            data = json.dumps({'type': 'give', 'data': 'test'})
+            data = json.dumps({'type': 'give', 'data': card})
             self.send(name, data)
         except Exception:
             pass
