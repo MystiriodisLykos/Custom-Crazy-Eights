e3f983e8e0c7736cab84aee5039837e59b06cd85
MystiriodisLykos <MystiriodisLykos@gmail.com>
2017-04-27 21:03:24 -0600

    ready tests
    ready should give back the player dictionary

--- uno.py
+++ uno.py
@@ -48,7 +48,7 @@ class UnoGame(object):
     def ready(self, name):
         self.players[name]['ready'] = True
         ready = all([player['ready'] for player in self.players.values()])
-        self.send(name)
+        self.send(name, json.dumps(self.players))
         # if ready:
         #     self.cast(json.dumps({'type': 'start', 'data': ''}))
 
