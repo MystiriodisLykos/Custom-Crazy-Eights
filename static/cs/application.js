94196b9afb2183772397d2b157f9f21fdfebd637
MystiriodisLykos <MystiriodisLykos@gmail.com>
2017-05-03 12:13:11 -0600

    Fixed file path to cards
    Fixed file paths to cards in the for loop of draw and add some
    clearStageis before tiggering ready to play because we need to

--- static/cs/application.js
+++ static/cs/application.js
@@ -154,7 +154,7 @@
   };
 
   draw = function() {
-    var card, cardStr, i, index, len, offset, style, style1, unableToPlay, welcomeToUno;
+    var card, cardStr, index, j, len, offset, style, style1, unableToPlay, welcomeToUno;
     clearStage();
     leftArr.anchor.set(.5);
     leftArr.scale.x = leftArr.scale.y = scale;
@@ -221,12 +221,12 @@
     faceDown.x = (window.innerWidth / 2) - 75;
     faceDown.y = (window.innerHeight / 2) - 75;
     app.stage.addChild(faceDown);
-    for (index = i = 0, len = ca.length; i < len; index = ++i) {
+    for (index = j = 0, len = ca.length; j < len; index = ++j) {
       cardStr = ca[index];
       if (index <= end && index >= start) {
         index -= start;
         offset = 5 - index;
-        start = '../static/assets/';
+        start = '../static/assets/uno cards';
         cardStr = start + cardStr.color + "_" + cardStr.value + ".png";
         card = PIXI.Sprite.fromImage(cardStr);
         card.anchor.set(.5);
@@ -324,6 +324,7 @@
       type: 'add',
       data: ''
     });
+    console.log(message);
     server.send(message);
   };
 
@@ -380,10 +381,10 @@
   };
 
   clearStage = function() {
-    var child, i, len, ref;
+    var child, j, len, ref;
     ref = app.stage.children;
-    for (i = 0, len = ref.length; i < len; i++) {
-      child = ref[i];
+    for (j = 0, len = ref.length; j < len; j++) {
+      child = ref[j];
       app.stage.removeChild(child);
     }
   };
@@ -417,12 +418,25 @@
   server = new ReconnectingWebSocket(ws_scheme + location.host + "/server");
 
   server.onmessage = function(message) {
+    var i;
     message = JSON.parse(message.data);
     console.log(message);
     switch (message.type) {
       case 'welcome':
         if (!(message.data in listDict)) {
           getName(message.data);
+          [
+            (function() {
+              var j, len, ref, results;
+              ref = range(3);
+              results = [];
+              for (j = 0, len = ref.length; j < len; j++) {
+                i = ref[j];
+                results.push(clearStage());
+              }
+              return results;
+            })()
+          ];
           readyToPlay();
         }
         break;
