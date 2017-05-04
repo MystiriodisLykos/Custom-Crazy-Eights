60878fe57809a4e3de37d34cee87dbf2ccd64384
MystiriodisLykos <MystiriodisLykos@gmail.com>
2017-05-04 10:35:16 -0600

    tests

--- uno.py
+++ uno.py
@@ -121,13 +121,14 @@ class UnoGame(object):
         self.turn_order = self.turn_order[1:] + [self.turn_order[0]]
 
     def play(self, name, data):
+        self.cast(json.dumps({ 'type':'test', 'data':'1' }))
         if data != '':
             self.players[name]['cards'] -= 1
             if self.players[name]['cards'] == 0:
                 self.gg(name)
             card = Card(data['color'], data['value'])
             self.discard.append(card)
-        self.cast(json.dump({'type': 'test'}))
+        self.cast(json.dumps({'type': 'test', 'data': '2'}))
         self.turn()
 
     def uno(self, name):
