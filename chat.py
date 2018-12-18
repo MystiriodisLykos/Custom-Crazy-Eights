646fffbb9c0acf8a3c6f52065209974156af9198
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2018-12-18 12:51:20 -0700

    Trying to set up Flask Blueprints with sockets

--- chat.py
+++ chat.py
@@ -1,25 +1,3 @@
-# -*- coding: utf-8 -*-
+import cce
 
-"""
-Chat Server
-===========
-This simple application uses WebSockets to run a primitive chat server.
-"""
-
-import os
-from flask import Flask, render_template
-
-app = Flask(__name__)
-app.debug = 'DEBUG' in os.environ
-
-@app.route('/')
-def hello():
-    return render_template('home.html')
-
-@app.route('/test/')
-def test():
-    return 'TEST'
-
-from views import chat
-print('test')
-app.register_blueprint(chat.mod)
+app = cce.create_app()
